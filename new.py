05bcb443eab59b216864a71fada3549f1d3ef918
nithin <nithindamarla21@gmail.com>
2025-06-07 13:57:14 +0530

    most popular num code version 3

--- new.py
+++ new.py
@@ -1,4 +1,13 @@
 import random
-print("hi")
-
-print(random.random())
+l=[]
+for i in range(100):
+    x=random.random()
+    l.append(round(x*100))
+d={}
+for i in l:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]+=1
+popular=max(d,key=d.get)
+print("frequent number is ",popular)
